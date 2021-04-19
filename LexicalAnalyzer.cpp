c208d8dd8bf97a56c84e28bb1887efe40322ece0
Carson Whitt <whittginger@gmail.com>
2021-04-18 21:16:53 -0700

    Work done on constructor. Destructor still needs work as well.

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -4,23 +4,65 @@
 
 using namespace std;
 
-//enum token_type {NONE = -1, EOF_T = 0, IDENT_T = 1, NUMLIT_T, NUM_TOKENS};
+// Move to header file
+enum token_type {NONE = -1, EOF_T = 0, IDENT_T = 100, NUMLIT_T, STRLIT_T, LISTOP_T, CONS_T, IF_T, COND_T, ELSE_T, DISPLAY_T, NEWLINE_T, AND_T, OR_T, NOT_T, DEFINE_T, LET_T, NUMBERP_T, LISTP_T, ZEROP_T, NULLP_T, STRINGP_T, PLUS_T, MINUS_T, DIV_T, MULT_T, MODULO_T, ROUND_T, EQUALTO_T, GT_T, LT_T, GTE_T, LTE_T, LPAREN_T, RPAREN_T, LBRACK_T, RBRACK_T, SQUOTE_T, ERROR_T, NUM_TOKENS};
+
 static string token_names[] = {	"EOF_T", "IDENT_T", "NUMLIT_T": }; 
 
 LexicalAnalyzer::LexicalAnalyzer (char * filename)
 {
 	// This function will initialize the lexical analyzer class
+	string filename_str = filename;
+	string listfile_str = filename_str + ".lst";
+	string tokenfile_str = filename_str + ".p1";
+	string dbfile_str = filename_str + ".dbg";
+	
+	// Try to open input file
+	input.open(filename_str);
+	if(!input){
+	       	cout << "Error: Input file " << filename << "couldn't be opened" << endl;
+		cout << "Terminating" << endl;
+		exit(-1);
+	}
+	listingFile.open(listfile_str , ios::out);
+	tokenFile.open(tokenfile_str, ios::out);
+	debugFile.open(dbfile_str, ios::out);
+	
+	// Init position and error count vars
+	errors = 0;
+	linenum = 1;
+	pos = 1;
+	
+	while(!input.eof()){
+		line = input.getline();
+		token = GetToken();
+		
+		
+	}
+
+
 }
 
 LexicalAnalyzer::~LexicalAnalyzer ()
 {
 	// This function will complete the execution of the lexical analyzer class
-}
+	debugFile << "Lexical Analyzer finished. " << errors << " recorded." << endl;
+	
+	input.close();
+	lsitingFile.close()
+	tokenFile.close();
+	debugFile.close();
+
+
+}	
 
 token_type LexicalAnalyzer::GetToken ()
 {
 	// This function will find the next lexeme int the input file and return
 	// the token_type value associated with that lexeme
+	
+	
+	
 	return token;
 }
 
