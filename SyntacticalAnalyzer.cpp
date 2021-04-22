b0c9df5512d31f668d7dd8686117d704698b366a
Carson Whitt <whittginger@gmail.com>
2021-04-21 22:51:25 -0700

    Project finsihed

--- SyntacticalAnalyzer.cpp
+++ SyntacticalAnalyzer.cpp
@@ -8,11 +8,17 @@ using namespace std;
 
 SyntacticalAnalyzer::SyntacticalAnalyzer (char * filename)
 {
+	cout << "SyntacticalAnalyzer using --> " << filename << endl;
 	lex = new LexicalAnalyzer (filename);
+	cout << "Created Lexical Analyzer" << endl;
 	token_type t;
 	while ((t = lex->GetToken()) != EOF_T)
 	{
+		
 	}
+
+	cout << "Lexical Analyzer Finished" << endl;
+
 }
 
 SyntacticalAnalyzer::~SyntacticalAnalyzer ()
